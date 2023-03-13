ce4ca524fa216e6ddebbde860ce5563fee8bacd8
Rizky Dharma Andika Raya <38689505+rizkydharma02@users.noreply.github.com>
2023-03-14 00:17:53 +0700

    tugas baru js

---
+++ tugas-postest/productzilla_D10_method_take_home_task/main.js
@@ -0,0 +1,201 @@
+let nama1;
+
+function setNama(nama1) {
+    nama = nama1;
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('1.rizky'))
+
+let nama2;
+function setNama(nama2) {
+    nama = nama2
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('2.dharma'))
+
+
+let nama3;
+function setNama(nama3) {
+    nama = nama3
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('3.andika'))
+
+let nama4;
+function setNama(nama4) {
+    nama = nama4
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('4.raya'))
+
+let nama5;
+function setNama(nama5) {
+    nama = nama5
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('5.farn'))
+
+let nama6;
+function setNama(nama6) {
+    nama = nama6
+}
+
+function getNama(nama) {
+    return nama
+}
+
+console.log(getNama('6.sisca'))
+
+let nama7;
+function setNama(nama7) {
+    nama = nama7
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('7.dwi'))
+
+let nama8;
+function setNama(nama8) {
+    nama = nama8
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('8.sukma'))
+
+let nama9;
+function setNama(nama9) {
+    nama = nama9
+}
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('9.raya'))
+
+let nama10;
+function setNama(nama10) {
+    nama = nama10
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('10.tiah'))
+
+let nama11;
+function setNama(nama11) {
+    nama = nama11
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('11.fifah'))
+
+let nama12;
+function setNama(nama12) {
+    nama = nama12
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('12.fitya'))
+
+let nama13;
+function setNama(nama13) {
+    nama = nama13
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('13.nugraha'))
+
+let nama14;
+function setNama(nama14) {
+    nama = nama14
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('14.persada'))
+
+let nama15;
+function setNama(nama15) {
+    nama = nama15
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('15.faisal'))
+
+let nama16;
+function setNama(nama16) {
+    nama = nama16
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('16.hanif'))
+
+let nama17;
+function setNama(nama17) {
+    nama = nama17
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('17.ficko'))
+
+let nama18;
+function setNama(nama18) {
+    nama = nama18
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('18.Gardien'))
+
+let nama19;
+function setNama(nama19) {
+    nama = nama19
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('19.putri'))
+
+let nama20;
+function setNama(nama20) {
+    nama = nama20
+}
+
+function getNama(nama) {
+    return nama
+}
+console.log(getNama('20.amelia'))
